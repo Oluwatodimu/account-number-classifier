2c61a485f7eec72548dabf43abf64658df812395
todimu <todimuisewon@gmail.com>
2022-09-04 00:28:15 +0100

    initial commit

---
+++ src/main/java/com/nuban/accountclassification/AccountClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.nuban.accountclassification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountClassificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountClassificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuban/accountclassification/AccountClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuban.accountclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountClassificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
